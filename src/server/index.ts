b92165452b2ab9748f8f959bfabb478839d44bd7
epifanov <epifanovmd@gmail.com>
2022-05-14 21:48:33 +0300

    change port

--- src/server/index.ts
+++ src/server/index.ts
@@ -13,7 +13,7 @@ const DEV = process.env.NODE_ENV === "development";
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8180;
 
 if (DEV) {
   (process.env as any).SSR = true;
